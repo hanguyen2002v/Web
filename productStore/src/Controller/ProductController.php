fed540fd5db2436f839827fc22ce4e9c48c87028
hanguyen2002v <109323029+hanguyen2002v@users.noreply.github.com>
2022-08-20 23:15:02 +0700

    Update ProductController.php

--- productStore/src/Controller/ProductController.php
+++ productStore/src/Controller/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
